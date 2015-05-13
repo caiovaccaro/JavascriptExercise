7911f08ee7283c6ad6737c1e90df4e4706212bb4
Leonardo Dino <leonardo@kunstdesign.com.br>
2015-05-12 21:55:31 -0300

    full ES6 update

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 
+import jQuery from 'jquery';
+import _TC from 'ember-template-compiler';
+import _Ember from 'ember';
+
 import setRouter from 'app/router';
 import * as Routes from 'routes/all';
 
