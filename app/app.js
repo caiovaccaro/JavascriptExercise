094c976fe8ab44f9b195cb29303a537ee7edf7ce
Leonardo Dino <leonardo@kunstdesign.com.br>
2015-05-12 22:45:19 -0300

    rename setRouter -> Router.setup

--- app/app.js
+++ app/app.js
@@ -4,12 +4,12 @@ import jQuery from 'jquery';
 import _TC from 'ember-template-compiler';
 import _Ember from 'ember';
 
-import setRouter from 'app/router';
+import Router from 'app/router';
 import * as Routes from 'routes/all';
 
 var init = function(){
 	var App = Ember.Application.create();
-	setRouter(App);
+	Router.setup(App);
 	_.assign(App, Routes);
 	
 	return App;
--- app/router.js
+++ app/router.js
@@ -1,11 +1,11 @@
-var setRouter = function(App){
-	App.Router.map(function() {
-		this.route('page', { path: '/*page_url' });
-	});
+export default {
+	setup: function(App){
+		App.Router.map(function() {
+			this.route('page', { path: '/*page_url' });
+		});
 
-	App.Router.reopen({
-	  location: 'auto'
-	});
+		App.Router.reopen({
+		  location: 'auto'
+		});
+	}
 }
-
-export default setRouter;
